7b20d4c9b005df44823a50167c4410916a54fea9
Matti Kortelainen <matti.kortelainen@cern.ch>
2024-02-02 08:44:22 -0600

    Update comment.cc

--- comment.cc
+++ comment.cc
@@ -42,3 +42,12 @@
  Usage: Produces data objects for ESProducers in EventSetup system
 
 */
+
+//
+/*
+
+ Description: Functional object used as the 'callback' for the CallbackESProductResolver
+
+ Usage: Produces data objects for ESProducers in EventSetup system
+
+*/
